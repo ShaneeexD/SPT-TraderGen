451c24e6cff7bd126b07b72efdb6859747bd22c3
ShaneeexD <31047364+ShaneeexD@users.noreply.github.com>
2026-06-17 20:27:01 +0100

    Add CommonLib as required mod dependency with version constraint ~2.0.0

--- Server/TraderGenPlugin.cs
+++ Server/TraderGenPlugin.cs
@@ -24,7 +24,10 @@ public record ModMetadata : AbstractModMetadata
     public override SemanticVersioning.Version Version { get; init; } = new("1.5.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("4.0.13");
     public override List<string>? Incompatibilities { get; init; }
-    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new("~2.0.0") }
+    };
     public override string? Url { get; init; }
     public override bool? IsBundleMod { get; init; } = false;
     public override string? License { get; init; } = "MIT";
